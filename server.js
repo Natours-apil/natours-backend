1711bfd8df4766ae269e42533f0126ee57ea2324
apilbasnet <apilbasnet10@gmail.com>
2025-04-01 11:23:14 +0545

    database connection

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
+  });
 
 const app = require('./app');
 
