dd2a19ae4da0f21e95b669268bf346266161b25c
apilbasnet <apilbasnet10@gmail.com>
2025-04-05 17:00:42 +0545

    removed previous old code after refactoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,32 +2,6 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 
-// locally importing data for testing purposes
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// const checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// const checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Name and Price must be included.',
-//     });
-//   }
-//   next();
-// };
-
 const aliasTopTours = (req, res, next) => {
   req.query.sort = 'price,-ratingsAverage';
   req.query.limit = '5';
@@ -38,62 +12,6 @@ const aliasTopTours = (req, res, next) => {
 
 const getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    // 1A) FILTERING
-    // const queryObj = { ...req.query };
-    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-    // excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) ADVANCED FILTERING
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // let query = Tour.find(JSON.parse(queryStr));
-    // {difficulty: 'easy', duration: { $gte: 5}}
-    // {difficulty: 'easy', duration: { gte: '5'}
-
-    // console.log(queryObj);
-    // console.log(JSON.parse(queryStr));
-
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    // 2) SORTING
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   console.log(sortBy);
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort('-createdAt');
-    // }
-
-    // 3) FIELD LIMITING
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // 4) PAGINATION
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit; // page=1&limit=10 => 0 , page=2&limit=10 => 10
-
-    // page=2&limit=10
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) {
-    //     throw new Error('This page does not exist');
-    //   }
-    // }
-
     // EXECUTING QUERY
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
