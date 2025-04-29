cf9febd7f33c5bd49eaf0936d736c882bd626597
apilbasnet <apilbasnet10@gmail.com>
2025-04-29 12:44:21 +0545

    err during dev vs prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,38 @@
+const sendErrorDev = (err, req, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  if (err.isOperational) {
+    // Operational, trusted error: send message to client
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error: don't leak error details
+    console.error('ERROR 💥', err);
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, req, res);
+  }
 };
