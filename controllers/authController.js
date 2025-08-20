c50a3723fe103e21a412f6dc309447181b7a0881
apilbasnet <apilbasnet10@gmail.com>
2025-08-20 15:51:21 +0545

    forgotpassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) => {
   console.log('JWT_EXPIRES_IN:', process.env.JWT_EXPIRES_IN);
@@ -116,6 +117,36 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
+  \nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    console.log(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'There was an error sending the email. Please try again later.',
+    });
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
