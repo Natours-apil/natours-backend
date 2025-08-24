c8c85737e9138361c5de0394405b224c61bfed75
apilbasnet <apilbasnet10@gmail.com>
2025-08-24 09:06:47 +0545

    user upadate

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,16 @@ const AppError = require('../utils/appError');
 // const Tour = require('../models/tourModel').default;
 // const APIFeatures = require('../utils/apiFeatures');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      newObj[key] = obj[key];
+    }
+  });
+  return newObj;
+};
+
 const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -15,6 +25,40 @@ const getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+const updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword instead.',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+const deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
@@ -58,4 +102,6 @@ module.exports = {
   getUser,
   updateUser,
   deleteUser,
+  updateMe,
+  deleteMe,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -51,6 +51,12 @@ const userSchema = new Schema({
   },
   passwordResetToken: String,
   passwordResetExpires: Date,
+
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -67,6 +73,11 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
