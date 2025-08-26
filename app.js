1b3a0835f38e779bfdf012a81e6018eb5fdff8fa
apilbasnet <apilbasnet10@gmail.com>
2025-08-26 14:01:03 +0545

    p parameter pollute

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -45,6 +46,20 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'price',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'maxGroupSize',
+    ],
+  })
+);
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
@@ -73,11 +88,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Cannot find ${req.originalUrl} on this server.`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-  // next(err);
-
   next(new AppError(`Cannot find ${req.originalUrl} on this server.`, 404));
 });
 
