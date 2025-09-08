fa0ec4b4783e666f0707f293559d0b7bd2804c00
apilbasnet <apilbasnet10@gmail.com>
2025-09-08 13:25:47 +0545

    default pp

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,6 +58,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
