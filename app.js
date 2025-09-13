047ea25935094f8e167010e0c0977eb3e48c3f1b
apilbasnet <apilbasnet10@gmail.com>
2025-09-13 12:43:09 +0545

    add cors

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ const { mongo } = require('mongoose');
 
 const app = express();
 
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 // 1)global MIDDLEWARES
 
 // Set security http headers
